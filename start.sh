8b8ad7446414b34fbd557ba5c41ad7cb1694adb3
Muhammad Elsaeed <muhammadalsaied96@gmail.com>
2021-12-23 21:14:55 +0200

    deploy the DNS cluster Add-on

--- start.sh
+++ start.sh
@@ -101,3 +101,13 @@ for i in 0 1 2; do
     --destination-range 10.200.${i}.0/24
 done
 gcloud compute routes list --filter "network: kubernetes-the-hard-way"
+
+print_headline "deploy the DNS cluster Add-on"
+kubectl apply -f https://storage.googleapis.com/kubernetes-the-hard-way/coredns-1.8.yaml
+kubectl get pods -l k8s-app=kube-dns -n kube-system
+print_comment "verification"
+kubectl run busybox --image=busybox:1.28 --command -- sleep 3600
+sleep 10
+kubectl get pods -l run=busybox
+POD_NAME=$(kubectl get pods -l run=busybox -o jsonpath="{.items[0].metadata.name}")
+kubectl exec -ti $POD_NAME -- nslookup kubernetes
